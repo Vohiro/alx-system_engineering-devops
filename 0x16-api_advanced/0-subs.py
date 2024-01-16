dee35404e82a9661b3e7e205e0de6dc18e847bfb
Vohiro <dvohiro@gmail.com>
2024-01-16 15:50:57 -0800

    function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""
- function that queries the Reddit API and returns the number of subscribers
-"""
+""" returns no of subscribers for a given subreddit"""
+
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/redditdev/about.json"
-    response = requests.get(url, allow_redirects=False)
-    if response.status_code == 200:
-        return response.json()["data"]["subscribers"]
-    elif response.status_code == 302:
-        print("Subreddit not found.")
-    else:
-        print("Unexpected status code: " + response.status_code)
-    return 0
+    """a function that queries the Reddit API and returns the number of
+    subscribers (not active users, total subscribers) for a given subreddit"""
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "Custom User"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
