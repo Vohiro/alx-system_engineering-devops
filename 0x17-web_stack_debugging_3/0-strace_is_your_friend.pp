f9f00010310766384858f2299d36673f81fc4316
Vohiro David <dvohiro@gmail.com>
2024-06-06 23:43:46 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,8 +0,0 @@
-#Puppet command to replace a string in a php config file
-
-#change "phpp" to  "php"
-
-exec { 'fix_error':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => ['/usr/bin', '/bin']
-}
